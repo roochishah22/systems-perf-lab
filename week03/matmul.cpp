459f0360c6008ec27d731dc4d38f66704648474d
Roochi Shah <roochi.shah22@gmail.com>
2026-04-04 23:47:54 -0700

    week 3: loop tilting experient gone wrong, hardware prefetcher analyis on M3 apple silicon

--- week03/matmul.cpp
+++ week03/matmul.cpp
@@ -51,6 +51,24 @@ void matmul_inlined(Matrix& A, Matrix& B, Matrix& C) {
                 C.data[i*N+j] += A.data[i*N+k] * B.data[k*N+j];
 }
 
+
+void matmul_tiled(Matrix& A, Matrix& B, Matrix& C, int tile_size) {
+    int N = A.N;
+    for (int i = 0; i < N; i += tile_size)
+        for (int k = 0; k < N; k += tile_size)
+            for (int j = 0; j < N; j += tile_size)
+                for (int ii = i; ii < i + tile_size; ii++) {
+                    int row_a = ii * N;
+                    int row_c = ii * N;
+                    for (int kk = k; kk < k + tile_size; kk++) {
+                        float a_val = A.data[row_a + kk];
+                        int row_b = kk * N;
+                        for (int jj = j; jj < j + tile_size; jj++)
+                            C.data[row_c + jj] += a_val * B.data[row_b + jj];
+                    }
+                }
+}
+
 void benchmark(const std::string& name, int N, 
                std::function<void()> fn) {
     auto start = std::chrono::high_resolution_clock::now();
@@ -61,8 +79,8 @@ void benchmark(const std::string& name, int N,
 }
 
 int main() {
-    for (int N : {256, 512}) {
-        Matrix A(N), B(N), C(N), C2(N), C3(N);
+    for (int N : {512, 1024, 2048}) {
+        Matrix A(N), B(N), C(N), C2(N), C3(N), C4(N), C5(N);;
 
         for (int i = 0; i < N; i++)
             for (int j = 0; j < N; j++) {
@@ -73,6 +91,8 @@ int main() {
         benchmark("naive",     N, [&]{ matmul_naive(A, B, C); });
         benchmark("reordered", N, [&]{ matmul_reordered(A, B, C2); });
         benchmark("inlined",   N, [&]{ matmul_inlined(A, B, C3); });
+        benchmark("tiled_16",  N, [&]{ matmul_tiled(A, B, C4, 16); });
+        benchmark("tiled_32",  N, [&]{ matmul_tiled(A, B, C5, 32); });
     }
     return 0;
 }
